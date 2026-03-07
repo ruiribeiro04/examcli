b6352bd0b95c6188a2244a45a20b648089cb3de3
ruiferna <ruiferna@student.42porto.com>
2026-03-07 12:51:37 +0000

    Implemented CF class `FileReader`: - Used `getline` to "read from file" (like get_next_line) - `ifstream`

---
+++ FileReader.cpp
@@ -0,0 +1,33 @@
+#include "FileReader.hpp"
+
+FileReader::FileReader(const FileReader &/*other*/) {
+    std::cout << "Copy constructor called\n";
+}
+
+FileReader &FileReader::operator=(const FileReader &/*other*/) {
+    std::cout << "Copy assignment operator called\n";
+    return *this;
+}
+
+FileReader::~FileReader() {
+    std::cout << "Destructor called\n";
+}
+
+std::string FileReader::read(const std::string &path) const {
+    std::ifstream file(path.c_str());
+    
+    std::string content;
+    std::string line;
+    
+    if (!file.is_open()) {
+        std::cerr << "Error: could not open file: " << path << "\n";
+        return "";
+    }
+    while (std::getline(file, line)) {
+        content += line;
+        content += "\n";
+    }
+
+    file.close();
+    return content;
+}
