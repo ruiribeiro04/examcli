c889721061e06b5df10aa01cab2a522682ee5a56
ruiferna <ruiferna@student.42porto.com>
2026-03-07 12:33:31 +0000

    COF barebones implementation of Result and Submission classes

---
+++ Result.hpp
@@ -0,0 +1,22 @@
+#ifndef RESULT_HPP
+#define RESULT_HPP
+
+#include <string>
+#include <iostream>
+
+class Result {
+private:
+    std::string _message;
+    bool _passed;
+
+public:
+    Result();
+    Result(const std::string &message, bool passed);
+    Result(const Result &other);
+    Result &operator=(const Result &other);
+    ~Result();
+
+    void show() const;
+};
+
+#endif
